5aa786317847f219a7a419340fa51d41e7a0fec1
cducote <chris.ducote89@gmail.com>
2020-09-01 01:00:11 -0400

    FIXED ITTTTT

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -1,11 +1,21 @@
 // users.js
 
 const { admin, db } = require('../util/admin');
-const config = require('../util/config');
+// const config = require('../util/config');
+const firebaseConfig = {
+    apiKey: "AIzaSyAcAx4wmBVZilwvs89TsgG4I2ZCNfOPYF8",
+    authDomain: "onlyfrags-a97bd.firebaseapp.com",
+    databaseURL: "https://onlyfrags-a97bd.firebaseio.com",
+    projectId: "onlyfrags-a97bd",
+    storageBucket: "onlyfrags-a97bd.appspot.com",
+    messagingSenderId: "739568356197",
+    appId: "1:739568356197:web:c4c70cda77bc4fc183ef8c",
+    measurementId: "G-3FCWYFF6Q5"
+  };
 
 const firebase = require('firebase');
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 const { validateLoginData, validateSignUpData } = require('../util/validators');
 
